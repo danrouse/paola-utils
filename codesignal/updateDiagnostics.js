22d8b55e7e1ed2a20b0de51e7b167d47f9015c35
stevenchung213 <stevenchung.213@gmail.com>
2022-10-12 12:31:24 -0700

    added codesignal into paola-utils

---
+++ codesignal/updateDiagnostics.js
@@ -0,0 +1,257 @@
+// import 'dotenv/config';
+// import { GoogleSpreadsheet } from 'google-spreadsheet';
+// import { getCodeSignalResults, formatResultObject } from "./helpers/index.js";
+
+// older node versions
+require('dotenv').config();
+const GoogleSpreadsheet = require('google-spreadsheet').GoogleSpreadsheet;
+const getCodeSignalResults = require('./helpers/index.js').getCodeSignalResults;
+const formatResultObject = require('./helpers/index.js').formatResultObject;
+
+// TODO - update list of CodeSignal testIDs if needed
+const mod1DiagID = 'TmxaK6kMj4WA5wah4';
+const mod2DiagID = 'hxYP2PGXNYR8FLSWz';
+const mod3DiagID = 'XLY9N2TuxbEkXPkqo';
+const seiDiagID = 'kb3zp9cK5CYLadq22';
+const cohortStartDate = new Date('9/5/2022');
+const seiDiagnosticStartDate = new Date('10/24/2022');
+const weekBeforeSei = seiDiagnosticStartDate.getDate() - 11;
+seiDiagnosticStartDate.setDate(weekBeforeSei);
+
+
+const sheetTabs = {
+  [mod1DiagID]: {
+    tabID: 399690295, // <- 399690295 is the googlesheets_tabId for 'CodeSignal
+    once: true,
+  },
+  [mod2DiagID]: {
+    tabID: 1733706020,
+    once: true,
+  },
+  [mod3DiagID]: {
+    tabID: 91576197,
+    once: true,
+  },
+  [seiDiagID]: {
+    tabID: 1737554240,
+    once: true,
+  },
+  'HRPTIV': {
+    tabID: 1952690505,
+    once: true,
+    allStudents: [],
+  },
+  'CESP': {
+    tabID: 0,
+    once: true,
+    allStudents: [],
+    studentEmails: {},
+  }
+};
+
+const updatePulseDiagnostic = async (diagID, first = 0, offset = 0, seiD = false) => {
+  const realPulseSheetID = process.env.GOOGLE_SHEETS_PULSE_SHEET;
+  const realCESPSheetID = process.env.GOOGLE_SHEETS_CESP_SHEET;
+
+  const pulse = new GoogleSpreadsheet(realPulseSheetID);
+  const cesp = new GoogleSpreadsheet(realCESPSheetID);
+
+  await pulse.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+  });
+
+  await cesp.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+  });
+
+
+  await pulse.loadInfo();
+  await cesp.loadInfo();
+
+  // if we want to create a new sheet, use .addSheet() + logic below
+  // const resultSheet = pulse.addSheet()
+  // const resultSheet = pulse.sheetsByIndex[0]; // sheetsByTitle?
+  const currentSheet = pulse.sheetsById[sheetTabs[diagID].tabID];
+
+  // only for SEI Diagnostics
+  if (seiD && sheetTabs.CESP.once) {
+    const cespRoster = cesp.sheetsByTitle.Roster;
+    const cespRows = await cespRoster.getRows();
+    const cespHeaders = cespRows[0]._sheet.headerValues;
+    cespRows.forEach(row => {
+      console.log(row._rawData)
+      const studentObj = {};
+      row._rawData.forEach((val, i) => {
+        studentObj[cespHeaders[i]] = val;
+      });
+      sheetTabs.CESP.allStudents.push(studentObj);
+      sheetTabs.CESP.studentEmails[studentObj['SFDC Email']] = {
+        name: studentObj['Full Name'],
+        campus: studentObj.Campus,
+        github: studentObj.GitHub,
+      }
+    });
+    sheetTabs.CESP.once = false;
+    console.log(sheetTabs.CESP.allStudents);
+    console.log(sheetTabs.CESP.studentEmails);
+  }
+
+
+  const result = await getCodeSignalResults(diagID, first, offset);
+
+  // format codesignal json for spreadsheet compatability
+  const allDiagnosticResults = result.data.companyTest.testSessions.map(test => (
+    seiD ? formatResultObject(test, sheetTabs.CESP.studentEmails) : formatResultObject(test)
+  ));
+
+  // filter out previous cohort results
+  // by cohortStartDate constant
+  const currentCohortResults = allDiagnosticResults.filter(studentObj => (
+    seiD ? new Date(studentObj.startDate).getTime() >= seiDiagnosticStartDate.getTime()
+      : new Date(studentObj.startDate).getTime() >= cohortStartDate.getTime()
+  ));
+
+  console.log(JSON.stringify(currentCohortResults, null, 2));
+
+  // only if filtered array has items
+  // if this condition doesn't run, there are no recent results
+  if (currentCohortResults.length) {
+    try {
+      // get headers for sheets -> email / fullName / githubHandle / ...
+      const headers = Object.keys(currentCohortResults[0]);
+      if (sheetTabs[diagID].once) {
+        // clear the sheet to prevent dupes
+        await currentSheet.clear();
+        await currentSheet.setHeaderRow(headers);
+        sheetTabs[diagID].once = false;
+      }
+      // set headers for sheet
+      // write results to sheet
+      await currentSheet.addRows(currentCohortResults);
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  console.log('No recent results');
+  return false;
+};
+
+// TODO - ...
+// maybe use # of currently enrolled students?
+// example case: currently housing 200 students
+// pull total # of students from CESP??
+// divide # of calls by 20 (max # of results)
+// chain subsequent calls
+// 10 total calls w/ 20 results each, totaling 200 results
+
+const updateM1Diagnostics = async () => {
+  try {
+    await updatePulseDiagnostic(mod1DiagID, 20, 0, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 20, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 40, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 60, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 80, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 100, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 120, false);
+    await updatePulseDiagnostic(mod1DiagID, 20, 140, false);
+    // await updatePulseDiagnostic(mod1DiagID, 20, 160, false);
+    // await updatePulseDiagnostic(mod1DiagID, 20, 180, false);
+  } catch (err) {
+    console.log(err)
+  }
+};
+
+const updateM2Diagnostics = async () => {
+  try {
+    await updatePulseDiagnostic(mod2DiagID, 20, 0, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 20, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 40, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 60, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 80, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 100, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 120, false);
+    await updatePulseDiagnostic(mod2DiagID, 20, 140, false);
+    // await updatePulseDiagnostic(mod2DiagID, 20, 160, false);
+    // await updatePulseDiagnostic(mod2DiagID, 20, 180, false);
+  } catch (err) {
+    console.log(err)
+  }
+};
+
+const updateM3Diagnostics = async () => {
+  try {
+    // await updatePulseDiagnostic(mod3DiagID, 0, 0, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 0, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 20, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 40, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 60, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 80, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 100, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 120, false);
+    await updatePulseDiagnostic(mod3DiagID, 20, 140, false);
+    // await updatePulseDiagnostic(mod3DiagID, 20, 160, false);
+    // await updatePulseDiagnostic(mod3DiagID, 20, 180, false);
+  } catch (err) {
+    console.log(err)
+  }
+};
+
+const updateSEIDiagnostics = async () => {
+
+  if (new Date().getTime() < seiDiagnosticStartDate.getTime()) {
+    console.log('NOT TIME FOR SEI DIAGNOSTICS');
+    return false;
+  }
+
+  try {
+    await updatePulseDiagnostic(seiDiagID, 20, 0, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 20, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 40, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 60, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 80, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 100, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 120, true);
+    await updatePulseDiagnostic(seiDiagID, 20, 140, true);
+    // await updatePulseDiagnostic(seiDiagID, 20, 160, true);
+    // await updatePulseDiagnostic(seiDiagID, 20, 180, true);
+  } catch (err) {
+    console.log(err)
+  }
+
+};
+
+const updateAllDiagnostics = async () => {
+  console.log('MODULE 1 DIAGNOSTICS');
+  updateM1Diagnostics()
+    .then(() => {
+      console.log('MODULE 1 COMPLETE');
+      console.log('MODULE 2 DIAGNOSTICS');
+      updateM2Diagnostics()
+    })
+    .then(() => {
+      console.log('MODULE 2 COMPLETE');
+      console.log('MODULE 3 DIAGNOSTICS')
+      updateM3Diagnostics()
+    })
+    .then(() => {
+      console.log('MODULE 3 COMPLETE');
+      console.log('SEI DIAGNOSTICS')
+      updateSEIDiagnostics()
+    })
+    .catch(err => {
+      console.log('Uh Oh');
+      console.log(err);
+    });
+};
+
+
+// updateM2Diagnostics();
+
+// updateM3Diagnostics();
+
+// updateSEIDiagnostics();
+
+updateAllDiagnostics();

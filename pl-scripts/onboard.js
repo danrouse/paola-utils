fd89116db9df575d6152738d464792adfa9866d0
Dan Rouse <dan@rouse.house>
2022-10-24 17:10:36 -0500

    onboard: set test to false, let's go!

--- pl-scripts/onboard.js
+++ pl-scripts/onboard.js
@@ -34,7 +34,7 @@ const {
 
 exitIfCohortIsNotActive();
 
-const TESTING_MODE = true;
+const TESTING_MODE = false;
 const MAX_STUDENTS_PER_RUN = 1;
 const PRODUCT_CODE_CAMPUS_OVERRIDES = {
   RFP: 'RFT Pacific',

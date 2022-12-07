0092d49e3da1005b3c4f2eca24ba022b6814ac01
Dan Rouse <dan@rouse.house>
2022-12-07 14:23:33 -0600

    updateCESP: remove pod assignment

--- pl-scripts/updateCESP.js
+++ pl-scripts/updateCESP.js
@@ -27,7 +27,6 @@ const CESP_ROSTER_SHEET_HEADERS = [
   'Tech Mentor',
   'Precourse Complete',
   'Status',
-  'RFP Pod',
 ];
 const CESP_MODULE_COMPLETION_SHEET_HEADERS = [
   'Full Name',
@@ -63,7 +62,6 @@ const formatStudentsForCESPRosterSheet = (students, separations) => students.map
   'Tech Mentor': student.techMentor,
   'Precourse Complete': student.allComplete,
   'Status': (separations.find((separatedStudent) => separatedStudent.fullName === student.fullName) || { separationType: 'Enrolled' }).separationType,
-  'RFP Pod': student['RFP Pod'],
 }));
 const formatStudentsForCESPModuleCompletionSheet = (students) => students.map((student) => ({
   'Full Name': student.fullName,
@@ -84,97 +82,6 @@ const filterAndSortStudents = (students) => students
   .sort(sortStudentsByDateAdded)
   .sort(sortStudentsByCampus);
 
-// Pods are assigned to RFP students by:
-// - creating two equally sized pods
-// - one pod contains all non-California residents
-// - attempts to keep average diagnostic scores equal
-// This function mutates the student objects passed in! ⚠
-const assignPods = async (repoCompletionStudents, rosterStudents) => {
-  const pacificStudents = repoCompletionStudents.filter((s) => s.campus === 'RFT Pacific')
-    .map((s) => {
-      const scores = [
-        s.m1DiagnosticTask1,
-        s.m1DiagnosticTask2,
-        s.m2DiagnosticTask1,
-        s.m2DiagnosticTask2,
-        s.m3DiagnosticTask1,
-        s.m3DiagnosticTask2,
-      ].filter((score) => score !== '-' && score !== undefined);
-      const hasDiagnostics = scores.length > 0;
-      const diagnosticAverage = scores.length > 0
-        ? scores.reduce((acc, cur) => acc + Number(cur), 0) / scores.length
-        : 0;
-
-      const rosterMatch = rosterStudents.find((rosterEntry) => rosterEntry.fullName.toLowerCase()
-        === s.fullName.toLowerCase());
-      const isCalifornia = !!(rosterMatch && (
-        rosterMatch.addressWhileInSchool.match(/california/i)
-        || rosterMatch.addressWhileInSchool.match(/\bCA\b/)
-      ));
-
-      return {
-        ...s,
-        isCalifornia,
-        hasDiagnostics,
-        diagnosticAverage,
-      };
-    });
-
-  const getPodAverage = (pod) => {
-    const a = pod.filter((s) => s.hasDiagnostics && s.diagnosticAverage);
-    return a.reduce((acc, cur) => acc + cur.diagnosticAverage, 0) / a.length;
-  };
-
-  const nonCaliPod = pacificStudents.filter((s) => !s.isCalifornia);
-  const caliPod = pacificStudents.filter((s) => s.isCalifornia);
-  let largerPodName = 'Only CA';
-  let smallerPodName = 'Includes Outside CA';
-  let largerPod = caliPod;
-  let smallerPod = nonCaliPod;
-  if (nonCaliPod.length > caliPod.length) {
-    largerPodName = 'Includes Outside CA';
-    smallerPodName = 'Only CA';
-    largerPod = nonCaliPod;
-    smallerPod = caliPod;
-  }
-  while (largerPod.length > smallerPod.length) {
-    let hasStudentsToMove = false;
-    const largerPodAverage = getPodAverage(largerPod);
-    const smallerPodAverage = getPodAverage(smallerPod);
-    for (let i = 0; i < largerPod.length; i += 1) {
-      if (largerPod[i].hasDiagnostics) {
-        if (
-          (largerPodAverage > smallerPodAverage && largerPod[i].diagnosticAverage > smallerPodAverage) ||
-          (largerPodAverage < smallerPodAverage && largerPod[i].diagnosticAverage < largerPodAverage)
-        ) {
-          const student = largerPod.splice(i, 1)[0];
-          smallerPod.push(student);
-          hasStudentsToMove = true;
-          break;
-        }
-      }
-    }
-    if (!hasStudentsToMove) {
-      break;
-    }
-  }
-  console.log(
-    'Created two pods with an average diagnostic score difference of',
-    Math.abs(getPodAverage(caliPod) - getPodAverage(nonCaliPod)),
-  );
-  repoCompletionStudents.forEach((student) => {
-    /* eslint-disable no-param-reassign */
-    if (largerPod.find((ts) => ts.githubHandle.toLowerCase() === student.githubHandle.toLowerCase())) {
-      student['RFP Pod'] = largerPodName;
-    } else if (smallerPod.find((ts) => ts.githubHandle.toLowerCase() === student.githubHandle.toLowerCase())) {
-      student['RFP Pod'] = smallerPodName;
-    } else {
-      student['RFP Pod'] = '';
-    }
-    /* eslint-enable no-param-reassign */
-  });
-};
-
 (async () => {
   console.info('Retrieving roster from Pulse...');
   const pulseSheet = await loadGoogleSpreadsheet(DOC_ID_PULSE);
@@ -190,8 +97,6 @@ const assignPods = async (repoCompletionStudents, rosterStudents) => {
   const repoCompletionStudentsNotSeparated = students.filter((student) => !separations.find(
     (separatedStudent) => separatedStudent.fullName === student.fullName,
   ));
-  const rosterStudents = await getRows(pulseSheet.sheetsByTitle.HRPTIV);
-  assignPods(repoCompletionStudentsNotSeparated, rosterStudents);
   const roster = formatStudentsForCESPRosterSheet(students, separations);
   const moduleCompletion = formatStudentsForCESPModuleCompletionSheet(repoCompletionStudentsNotSeparated);
 
